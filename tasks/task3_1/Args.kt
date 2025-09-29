63487ef69ff7b4a83402931bb747cab9a2d32d70
kieroxide <kieranbprivate@gmail.com>
2025-09-29 16:30:49 +0100

    completed tasks 3.1

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,10 @@
+import kotlin.system.exitProcess
 // Task 3.1: command line arguments
+fun main(args: Array<String>){
+    if (args.size < 2){
+        exitProcess(1)
+    }
+
+    println(args[0])
+    println(args[1])
+}

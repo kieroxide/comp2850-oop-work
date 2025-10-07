2b6e1ce96d349c017891ee5f281469c33efe3f8d
Kieran B <kieranbprivate@gmail.com>
2025-10-07 13:31:16 +0000

    Created unit testing of anagrams program finished task6_4_2

--- tasks/task6_4_2/src/test/kotlin/AnagramTest.kt
+++ tasks/task6_4_2/src/test/kotlin/AnagramTest.kt
@@ -6,5 +6,19 @@ import io.kotest.matchers.shouldBe
 
 @Suppress("unused")
 class AnagramTest: StringSpec({
-    // Write your unit tests here!
+    "Check anagram of two empty strings "{
+        withClue("Empty string is an anagram of Empty String") {"".anagramOf("") shouldBe true}
+    }
+    
+    "Check anagram of two different size strings" {
+        withClue("dog is not an anagram of dogdog") {"dog".anagramOf("dogdog") shouldBe false}
+    }
+
+    "Check anagram of same sized strings where they are not anagrams" {
+        withClue("cart is not an anagram of dart") {"cart".anagramOf("dart") shouldBe false}
+    }
+
+    "Check anagram of same sized strings where they are anagrams" {
+        withClue("cart is an anagram of ract") {"cart".anagramOf("ract") shouldBe true}
+    }
 })

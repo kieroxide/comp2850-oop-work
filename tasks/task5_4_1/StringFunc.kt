eb1607a4bf94186af88c4fd3a17a314c01e95dd8
Kieran B <kieranbprivate@gmail.com>
2025-10-07 12:47:27 +0000

    Finished task5_4_1

--- tasks/task5_4_1/StringFunc.kt
+++ tasks/task5_4_1/StringFunc.kt
@@ -1,1 +1,8 @@
 // Task 5.4.1: string extension function
+val String.tooLong: Boolean get() =  this.length > 20
+
+fun main(){
+    print("Enter a string: ");
+    val str: String = readln();
+    print("It is ${str.tooLong} that this string is too long");
+}

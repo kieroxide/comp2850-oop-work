7424a8b7acc7b0544b1fcc9c42694d7a9e7c1f84
Kieran B <kieranbprivate@gmail.com>
2025-10-07 11:43:04 +0000

    Finished anagrams

--- tasks/task5_1_1/Anagrams.kt
+++ tasks/task5_1_1/Anagrams.kt
@@ -1,1 +1,21 @@
 // Task 5.1.1: anagram checking using a function
+fun main(){
+    println("Input First word: ");
+    val first = readln();
+    println("\nInput Second word:");
+    val second = readln();
+    if(anagrams(first, second)){
+        println("\n$first is an anagram of $second");
+    } else {
+        println("\n$first is not an anagram of $second");
+    }
+}
+
+fun anagrams(first: String, second: String): Boolean {
+    if (first.length != second.length) {
+        return false;
+    }
+    val firstChars = first.lowercase().toList().sorted();
+    val secondChars = second.lowercase().toList().sorted();
+    return firstChars == secondChars;
+}

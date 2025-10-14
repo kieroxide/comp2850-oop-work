4475fefb3cad2991a0218a2c01e8f757203264dc
Kieran B <kieranbprivate@gmail.com>
2025-10-14 14:12:17 +0000

    All of task 10 completed

--- tasks/task10_3_2/Check2.kt
+++ tasks/task10_3_2/Check2.kt
@@ -1,11 +1,10 @@
 // Task 10.3.2: another example of an explicit null check
 
-fun printReversed(text: String) {   // modify parameter list
-
-    // modify function body to perform a null check
-
-    val result = text.reversed().uppercase()
-    println("Result: $result")
+fun printReversed(text: String?) {
+    when (text) {
+        null -> println("Result: null")
+        else -> println("Result: ${text.reversed().uppercase()}")
+    }
 }
 
 fun main() {
---
+++ tasks/task10_5/Elvis.kt
@@ -0,0 +1,14 @@
+// Task 10.3.2: another example of an explicit null check
+
+fun printReversed(text: String?): String{
+    return text?.let {it.reversed().uppercase()} ?: "???"
+}
+
+fun main() {
+    print("Enter a string: ")
+    val input = readLine()
+
+    println("Input: $input")
+    val res = printReversed(input)
+    println("Result: $res")
+}
